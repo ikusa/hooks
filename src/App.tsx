3f6301d799031b08d162796c6425f775f70eadd3
Joshua <joshuasetiawan04@gmail.com>
2019-09-30 16:39:25 +0700

    lifecycle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+type Recipe = {
+  title: string;
+};
+type State = {
+  recipes: Array<Recipe>;
+  searchQuery: string;
+};
+export default class App extends Component<{}, State> {
+  state: State = {
+    recipes: [],
+    searchQuery: '',
+  };
+  async componentDidMount() {
+    let res = await fetch('http://www.recipepuppy.com/api/');
+    let data = await res.json();
+    let recipes: Array<Recipe> = data.results;
+    this.setState({ recipes });
+  }
+
+  async componentDidUpdate(_: {}, prevState: State) {
+    if (prevState.searchQuery !== this.state.searchQuery) {
+      console.log('i do fetch');
+      let res = await fetch(
+        `http://www.recipepuppy.com/api/?q=${this.state.searchQuery}`,
+      );
+      let data = await res.json();
+      let recipes: Array<Recipe> = data.results;
+      this.setState({
+        recipes,
+      });
+    }
+  }
+
+  onChangeText = (text: string) => {
+    this.setState({ searchQuery: text });
+  };
+  render() {
+    console.log('render');
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+            width: 300,
+            padding: 5,
+          }}
+          onChangeText={(text) => this.onChangeText(text)}
+          value={this.state.searchQuery}
+        />
+
+        {this.state.recipes.map((recipe) => {
+          return <Item title={recipe.title} key={recipe.title}></Item>;
+        })}
+      </View>
+    );
+  }
+}
+
+function Item({ title }: Recipe) {
+  return <Text style={{ fontSize: 20 }}>- {title}</Text>;
 }
 
 const styles = StyleSheet.create({
